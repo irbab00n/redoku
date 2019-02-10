0338abbcb2ff87780eb73d69a0571e95296320ce
Thomas Cosby <cosbyts@gmail.com>
2019-02-09 22:58:47 -0800

    Prevent multiple submissions from occurring upon successful win state

--- client/src/components/Sudoku/PuzzleSubmission.jsx
+++ client/src/components/Sudoku/PuzzleSubmission.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 const PuzzleSubmission = ({ checkSolutionFunction, puzzle }) => (
   <div className="puzzle-submission-wrapper">
     <div className="inner-wrapper">
-      <button
-        className="puzzle-submission"
-        onClick={() => checkSolutionFunction(JSON.parse(JSON.stringify(puzzle.matrix)))}
-      >
-        Submit Puzzle
-      </button>
+      {
+        puzzle.winState ?
+          null :
+          <button
+            className="puzzle-submission"
+            onClick={() => checkSolutionFunction(JSON.parse(JSON.stringify(puzzle.matrix)))}
+          >
+            Submit Puzzle
+          </button>
+      }
     </div>
     {
       puzzle.submissionMessage !== '' ?
